30c9563914c8cb0212c999217418ae82dfa5c990
Alecia Dezzani <adezzani@bren.ucsb.edu>
2026-05-28 11:13:25 -0700

    fixed bridge repair function

--- R/bridge_repair_cost.R
+++ R/bridge_repair_cost.R
@@ -1,14 +1,19 @@
 #' Cost of Bridge Repairs due to flooding 
 #'
 #' This function calculates the total cost of bridge repairs based on the extent of local flooding.
-#' @param spent estimated cost of repairs needed per flooding event ($USD) default = $1 million
+#' @param costperflood estimated cost of repairs needed per flooding event ($USD) default = $1 million
 #' @param  floods number of flooding events per year based on data
-#' @return cost (annual) ($USD)
+#' @param discount discount rate (no unit) default = 7%
+#' @param time number of years evaluation default = 30 years
+#' @return npv (USD) net present value
 #'
 # function definition
-bridge_repair_cost = function(floods, spent = 1000000) {
+bridge_repair_cost = function(floods, 
+                              costperflood = 1000000, 
+                              discount = 0.07,
+                              time = 30) {
   # calculate annual repair cost
-  cost = floods * spent
-  return(cost)
+  npv = ((floods * costperflood) / (1 + discount)**time)
+  return(npv)
 }
 
--- R/fatal_cost.R
+++ R/fatal_cost.R
@@ -4,13 +4,15 @@
 #' @param vsl value of a statistical life (USD) default = 13000000 
 #' @param crashcount number of fatalities per year due to flooding (count)
 #' @param discount discount rate (no unit) default = 7%
+#' @param time number of years evaluation default = 30 years
 #' @return npv (USD) net present value
 #'
 # function definition
 fatal_cost = function(crashcount, 
                       vsl = 7400000, 
-                      discount = 0.07) {
+                      discount = 0.07,
+                      time = 30) {
   # calculate net present value
-  npv = ((crashcount * vsl) / discount) 
+  npv = ((crashcount * vsl) / (1 + discount)**time) 
   return(npv)
 }
