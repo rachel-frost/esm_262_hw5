c972fa86015d6cc016fc4c7dd8cdf89660184551
Alecia Dezzani <adezzani@bren.ucsb.edu>
2026-06-07 10:01:13 -0700

    more organization

--- R/count_flood_days.R
+++
@@ -1,9 +0,0 @@
-count_flood_days <- function(river_height_data, flood_threshold = 20) {
-  flood_days <- 0
-  for (i in 1:nrow(river_height_data)) {
-    if (river_height_data$river_ht[i] > flood_threshold) {
-      flood_days <- flood_days + 1
-    }
-  }
-  return(flood_days)
-}
